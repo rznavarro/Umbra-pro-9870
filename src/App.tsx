6605101761c173614abf3b5a11b7d4a096f51635
rznavarro <164783292+rznavarro@users.noreply.github.com>
2025-08-09 11:14:12 -0400

    test webhook commit

--- src/App.tsx
+++ src/App.tsx
@@ -44,14 +44,14 @@ const tools: Tool[] = [
     id: 4,
     name: "Simulador Legal",
     category: "Investigación",
-    webhookUrl: "https://8f7056be6e10.ngrok-free.app/webhook/Simulador%20de%20Problemas%20Legales",
+    webhookUrl: "https://8f7056be6e10.ngrok-free.app/webhook-test/Simulador%20de%20Problemas%20Legales",
     chatType: 'research'
   },
   {
     id: 5,
     name: "Normativas Locales",
     category: "Búsqueda",
-    webhookUrl: "https://8f7056be6e10.ngrok-free.app/webhook/Buscador%20de%20Normativas%20Locales",
+    webhookUrl: "https://8f7056be6e10.ngrok-free.app/webhook-test/Buscador%20de%20Normativas%20Locales",
     chatType: 'research'
   },
   {
